db4825ba20ee2875306d2ec73c199db1efa0d7e7
NSJ <95945513+MIKNSJ@users.noreply.github.com>
2023-12-21 00:50:44 -0800

    Create attempt_1.cpp

---
+++ 27_remove_element_easy/attempt_1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        unsigned int nonElements = 0;
+        std::vector<int>::iterator itr = nums.begin();
+
+            while (itr != nums.end()) {
+                if (*itr == val) {
+                    nums.erase(itr);
+                } else {
+                    nonElements++;
+                    itr++;
+                }
+            }
+
+        return nonElements;
+    }
+};
