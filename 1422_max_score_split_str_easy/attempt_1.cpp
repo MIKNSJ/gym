6e2f40532ea2212b231e97a923f897c27c14af6e
NSJ <95945513+MIKNSJ@users.noreply.github.com>
2023-12-22 12:52:31 -0800

    Create attempt_1.cpp

---
+++ 1422_max_score_split_str_easy/attempt_1.cpp
@@ -0,0 +1,39 @@
+#include <string>
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int leftScore = 0;
+        int rightScore = 0;
+        int score = 0;
+        int max_score = 0;
+        int split = 1;
+
+        while (split < s.length()) {
+            for (int i = 0; i < split; i++) {
+                if (s[i] == '0') {
+                    leftScore++;
+                }
+            }
+
+            for (int j = split; j < s.length(); j++) {
+                if (s[j] == '1') {
+                    rightScore++;
+                }
+            }
+
+            score = leftScore + rightScore;
+
+            if (max_score < score) {
+                max_score = score;
+            }
+
+            split++;
+            leftScore = 0;
+            rightScore = 0;
+            score = 0;
+        }
+
+        return max_score;
+    }
+};
