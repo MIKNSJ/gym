0e359b920bba1570218a7a233a13390867f7ec54
NSJ <95945513+MIKNSJ@users.noreply.github.com>
2023-12-23 11:59:18 -0800

    Create attempt_1.cpp

---
+++ 237_delete_node_link_list_medium/attempt_1.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* curr = node;
+        ListNode* prev = curr;
+
+        while (curr->next != nullptr) {
+            prev = curr;
+            curr = curr->next;
+            prev->val = curr->val;
+        }
+
+        prev->next = nullptr;
+    }
+};
