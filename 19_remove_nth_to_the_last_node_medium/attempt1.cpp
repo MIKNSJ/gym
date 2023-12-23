43bef75eaa7949dead28a5eb7933562e08340cf2
NSJ <95945513+MIKNSJ@users.noreply.github.com>
2023-12-23 01:19:35 -0800

    Create attempt1.cpp

---
+++ 19_remove_nth_to_the_last_node_medium/attempt1.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* curr = head;
+        ListNode* prev = curr;
+        int idx = 0;
+        int size = 1;
+
+        if (head->next == nullptr) {
+            delete head;
+            return nullptr;
+        }
+
+        while (curr->next != nullptr) {
+            curr = curr->next;
+            size++;
+        }
+
+        if (size - n == 0) {
+            head = head->next;
+            return head;
+        }
+
+        curr = head;
+        while (curr != nullptr) {
+            if (idx == size - n) {
+                prev->next = curr->next;
+                break;
+            }
+
+            prev = curr;
+            curr = curr->next;
+            idx++;
+        }
+
+        return head;
+    }
+};
