f28a4731fc5845acbc11e512e8ef42e66b61119c
NSJ <95945513+MIKNSJ@users.noreply.github.com>
2023-12-25 09:50:35 -0800

    Create attempt_1.cpp

---
+++ leetcode/00344_reverse_string_easy/attempt_1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        char copy;
+
+        while (left <= right) {
+            copy = s[left];
+            s[left] = s[right];
+            s[right] = copy;
+            left++;
+            right--;
+        }
+    }
+};
