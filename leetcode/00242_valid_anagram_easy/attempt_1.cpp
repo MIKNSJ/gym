2599b684944df48fd85aabe72136e52b92e479ea
NSJ <95945513+MIKNSJ@users.noreply.github.com>
2023-12-29 21:21:45 -0800

    Create attempt_1.cpp

---
+++ leetcode/00242_valid_anagram_easy/attempt_1.cpp
@@ -0,0 +1,15 @@
+// December 29, 2023
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if (s == t) {
+            return true;
+        }
+
+        return false;
+    }
+};
