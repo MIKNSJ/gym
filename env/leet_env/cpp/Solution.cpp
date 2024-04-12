ef52c7a418ed088a72dc4ec5d50cddf7a24a8a23
MIKNSJ <sculetoncode@gmail.com>
2024-04-12 16:25:04 -0700

    leet_env updated
    Update Makefile to also include -g

---
+++ env/leet_env/cpp/Solution.cpp
@@ -0,0 +1,42 @@
+// Date: YYYY-MM-DD
+
+#include "Solution.hpp"
+
+
+
+class Solution {
+    public:
+        /**
+         * Test function, DELETE WHEN EDITING
+         * @return nothing
+        */
+        void leet() {
+            cout << "This is Leetcode!" << endl;
+        }
+
+
+
+        /**
+         * TODO: Replace this line(s) with the description of the function
+         * to be implemented.
+         * @param
+         * @param
+         * @return
+        */
+        // TODO: Replace this line(s) with your implementation.
+};
+
+
+
+/**
+ * Universal cpp/c++ main function
+ * @return If main() ran succesfully return 0, otherwise 1.
+*/
+int main() {
+    Solution solution;
+    solution.leet(); // calling test function, DELETE WHEN EDITING
+
+    // TODO: Replace with the implemented function call.
+
+    return 0;
+}
--- env/leet_env/cpp/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include <set>
-#include <queue>
-#include <map>
-#include <unordered_map>
-#include <string>
-using namespace std;
