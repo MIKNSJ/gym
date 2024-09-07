a2d9acd959a7df300aae24b4dd5bd31c6a89edb5
MIKNSJ <sculetoncode@gmail.com>
2024-09-06 17:28:18 -0700

    leet 142 [1] and renamed path 701

---
+++ leetcode/problems/0000242-linked-list-cycle-ii/solution_1.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* slow_two = head;
+        ListNode* fast = head;
+
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        while (true) {
+            if (fast->next != nullptr) {
+                fast = fast->next;
+
+                if (fast->next != nullptr) {
+                    fast = fast->next;
+                } else {
+                    return nullptr;
+                }
+            } else {
+                return nullptr;
+            }
+
+            slow = slow->next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        while (true) {
+            if (slow == slow_two) {
+                break;
+            }
+
+            slow = slow->next;
+            slow_two = slow_two->next;
+        }
+
+        return slow;
+    }
+};
