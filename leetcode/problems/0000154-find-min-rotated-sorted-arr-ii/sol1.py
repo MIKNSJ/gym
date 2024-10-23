cf2a1a1c652d562ebc02f510f35cda9866eb09b2
MIKNSJ <sculetoncode@gmail.com>
2024-10-22 20:45:05 -0700

    leet 153 [2] and leet 154 [1]

---
+++ leetcode/problems/0000154-find-min-rotated-sorted-arr-ii/sol1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if (len(nums) == 1):
+            return nums[0]
+        
+        left = 0
+        right = len(nums) - 1
+
+        if (nums[left] < nums[right]):
+            return nums[left]
+        
+        while left < right:
+            leftValue = nums[left]
+            rightValue = nums[right]
+            midIdx = (left + right) // 2
+            midValue = nums[midIdx]
+
+            if midValue > rightValue:
+                left = midIdx + 1
+            elif midValue == leftValue and midValue == rightValue:
+                left+=1
+            else:
+                right = midIdx
+               
+        leftValue = nums[left]
+        return leftValue
